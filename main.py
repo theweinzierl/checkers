d784d5affec5f2a8293e01309b445d1d111c2675
theweinzierl <michael.weinzierl@posteo.de>
2022-01-21 15:07:09 +0100

    clean up

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
-from turtle import width
 from game.Board import *
 from game.Stone import *
 from tkinter import *
 
-
-
-
-
 window = Tk()
 window.title("Checkers")
-
 canvas = Canvas(window, width=400, height=400)
-
-
-
-test = Board(canvas)
-canvas.bind("<Button-1>", test.onClick)
-
-test.start()
-test.draw()
+board = Board(canvas)
+canvas.bind("<Button-1>", board.onClick)
+board.start()
+board.draw()
 canvas.pack()
 window.mainloop()
